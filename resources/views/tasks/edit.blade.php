d0ae6776025172042ad523a22b04579cfd784352
EC2 Default User <ec2-user@ip-172-31-5-29.us-east-2.compute.internal>
2018-05-29 03:38:52 +0000

    tasks

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.app')
+
+@section('content')
+
+    <h1>id: {{ $task->id }} のメッセージ編集ページ</h1>
+@endsection
---
+++ resources/views/tasks/show.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.app')
+
+@section('content')
+
+    <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
+
+    <p>{{ $task->content }}</p>
+
+@endsection
