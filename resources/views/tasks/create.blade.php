064432e33b92d0029e9c5e5bb8ee7bb7d62d756c
EC2 Default User <ec2-user@ip-172-31-5-29.us-east-2.compute.internal>
2018-06-01 03:02:29 +0000

    kadai-bootstrap

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -5,11 +5,27 @@
 <h1>New tasklist Page</h1>
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
+    <div class="row">
+    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+        {!! Form::model($task, ['route'=>'tasks.store']) !!}
+            <div class="form-group">
+                {!! Form::label('content', 'Task') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('status', 'Status') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
+    </div>
+</div>
 
         {!! Form::label('status', 'ステータス:') !!}
         {!! Form::text('status') !!}
-        {!! Form::label('content', 'task:') !!}
+        
+        
+        {!! Form::label('content', 'tasklist:') !!}
         {!! Form::text('content') !!}
 
         {!! Form::submit('Submit') !!}
--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -2,17 +2,25 @@
 
 @section('content')
 
-    <h1>id: {{ $tasks->id }} のメッセージ編集ページ</h1>
+    <h1>id: {{ $tasks->id }} のメッセージ編集ページ</h1><div class="row">
+   
+  <div class="row">
+    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+        {!! Form::model($tasks, ['route'=>'tasks.store']) !!}
+            <div class="form-group">
+                {!! Form::label('content', 'task') !!}
+                {!! Form::text('content', null, ['class' => 'form-control']) !!}
+            </div>
+            <div class="form-group">
+                {!! Form::label('status', 'status') !!}
+                {!! Form::text('status', null, ['class' => 'form-control']) !!}
+            </div>
+        {!! Form::submit('Submit', ['class' => 'btn btn-primary']) !!}
+        {!! Form::close() !!}
+    </div>
+</div>
+    
     
 
 
-    {!! Form::model($tasks, ['route' => ['tasks.update', $tasks->id], 'method' => 'put']) !!}
-　　　　 {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-        {!! Form::label('content', 'tasklist:') !!}
-        {!! Form::text('content') !!}
-
-        {!! Form::submit('refresh') !!}
-
-    {!! Form::close() !!}
 @endsection
