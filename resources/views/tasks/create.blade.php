df01c8a55957724af571cd9649159236ec8010a2
EC2 Default User <ec2-user@ip-172-31-5-29.us-east-2.compute.internal>
2018-06-01 05:36:58 +0000

    kadai-tasklist3

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -6,7 +6,7 @@
 
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
     <div class="row">
-    <div class="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
         {!! Form::model($task, ['route'=>'tasks.store']) !!}
             <div class="form-group">
                 {!! Form::label('content', 'Task') !!}
