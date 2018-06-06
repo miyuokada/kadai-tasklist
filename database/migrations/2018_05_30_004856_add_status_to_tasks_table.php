1cd37a9a561e81a6405ee17865bf6f521a62ebbe
EC2 Default User <ec2-user@ip-172-31-5-29.us-east-2.compute.internal>
2018-06-06 04:03:23 +0000

    miyukadai

--- database/migrations/2018_05_30_004856_add_status_to_tasks_table.php
+++ database/migrations/2018_05_30_004856_add_status_to_tasks_table.php
@@ -14,6 +14,7 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
+             
             $table->string('status');
         });
     }
